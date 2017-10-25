9661b7962c8a3b76cbe64418ef3e9de1da3ed050
akasai <ksj21c@naver.com>
2017-10-25 23:06:50 +0900

    Create 2293.java

---
+++ BaekJoon/2293.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+		
+		int cnt = sc.nextInt();
+		int ret = sc.nextInt();
+		int coins[] = new int[cnt];
+		
+		
+		for(int i = 0; i < cnt; i++)
+			coins[i] = sc.nextInt();
+		
+		System.out.println(Coin(coins, ret));
+    }
+    private static int Coin(int[] c, int max){
+		int DP[] = new int[max+1];
+		DP[0] = 1;
+		for(int i = 0; i < c.length; i++){
+			for(int n = 1; n <= max; n++){
+				if(n-c[i] >= 0)	DP[n] += DP[n-c[i]];
+			}
+		}
+		return DP[max];
+	}
+}
