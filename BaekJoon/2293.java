dbca3d6dda95f153351207a373cc0f958442c566
akasai <ksj21c@naver.com>
2017-10-25 23:56:07 +0900

    Update 2293.java

--- BaekJoon/2293.java
+++ BaekJoon/2293.java
@@ -1,25 +1,26 @@
 import java.util.Scanner;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-		
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+
 		int cnt = sc.nextInt();
 		int ret = sc.nextInt();
 		int coins[] = new int[cnt];
-		
-		
+
+
 		for(int i = 0; i < cnt; i++)
-			coins[i] = sc.nextInt();
-		
+		coins[i] = sc.nextInt();
+
 		System.out.println(Coin(coins, ret));
-    }
-    private static int Coin(int[] c, int max){
+	}
+	private static int Coin(int[] c, int max){
 		int DP[] = new int[max+1];
-		DP[0] = 1;
+		DP[0] = 1;	//초기화
+		
 		for(int i = 0; i < c.length; i++){
 			for(int n = 1; n <= max; n++){
-				if(n-c[i] >= 0)	DP[n] += DP[n-c[i]];
+				if(n-c[i] >= 0)	DP[n] += DP[n-c[i]];	//누적된 총 경우의 수이므로 +=연산
 			}
 		}
 		return DP[max];
