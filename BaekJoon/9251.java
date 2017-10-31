a62c3b55f29e8ae7c1e3df46c6970b67815e820c
akasai <ksj21c@naver.com>
2017-10-31 16:11:48 +0900

    Update 9251.java

--- BaekJoon/9251.java
+++ BaekJoon/9251.java
@@ -1,24 +1,24 @@
 import java.util.Scanner;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner s = new Scanner(System.in);
-		
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+
 		String str1 = s.next();
 		String str2 = s.next();
-		
+
 		System.out.println(findLCS(str1, str2));
-    }
-    private static int findLCS(String a, String b){
+	}
+	private static int findLCS(String a, String b){
 		int DP[][] = new int[b.length()+1][a.length()+1];
-		
+
 		for(int i = 1; i <= b.length(); i++){
 			for(int j = 1; j <= a.length(); j++){
 				if(a.charAt(j-1) == b.charAt(i-1))
 					DP[i][j] = DP[i-1][j-1] + 1;
 				else{
 					if(DP[i][j-1] > DP[i-1][j])	DP[i][j] = DP[i][j-1];
-					else						DP[i][j] = DP[i-1][j];
+					else				DP[i][j] = DP[i-1][j];
 				}
 			}
 		}
