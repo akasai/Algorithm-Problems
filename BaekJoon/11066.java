fa9a7995bf9e6d137c99f1774d232ab6a4996b1b
akasai <ksj21c@naver.com>
2017-11-08 20:47:40 +0900

    Update 11066.java

--- BaekJoon/11066.java
+++ BaekJoon/11066.java
@@ -1,44 +1,44 @@
 import java.util.Scanner;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner s = new Scanner(System.in);
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
 		int caseCNT = s.nextInt();
-		
+
 		for(int i = 0; i < caseCNT; i++){
 			int numCNT = s.nextInt();
 			int testCase[] = new int[numCNT];
-			
+
 			for(int j = 0; j < numCNT; j++)	testCase[j] = s.nextInt();
-            
+
 			System.out.println(solution(testCase));
 		}
-    }
-    public static int MIN(int a, int b){
+	}
+	public static int MIN(int a, int b){
 		return a <= b? a:b;
 	}
-    public static int sum(int[] a, int s, int e){
+	public static int sum(int[] a, int s, int e){
 		if(s == 0)    return a[e];
 		else          return a[e] - a[s-1];
 	}
-    private static int solution(int[] a){
+	private static int solution(int[] a){
 		int size = a.length;
-		int DP[][] = new int[size][size];
-		int s[] = new int[size];
-		
+		int DP[][] = new int[size][size];	//i~j까지 최소합 DP
+		int s[] = new int[size];	//SUM 저장
+
+		//i요소까지 총합
 		s[0] = a[0];
 		for(int i = 1; i < size; i++) s[i] += s[i-1] + a[i];
-		
+		//초기값 저장
 		for(int i = 0; i < size-1; i++) DP[i][i+1] = a[i] + a[i+1];
-		
-		for(int gap = 2; gap < size; gap++){
-			for(int i = 0; i+gap < size; i++){
-				int j = i+gap;
-				DP[i][j] = Integer.MAX_VALUE;
-				
-				for(int k = i; k < j; k++){
+
+		for(int gap = 2; gap < size; gap++){	//i와 j간 gap 3칸부터
+			for(int i = 0; i+gap < size; i++){	//i인덱스
+				int j = i+gap;	//j인덱스
+				DP[i][j] = Integer.MAX_VALUE;	//MIN을 구하기 위해
+
+				for(int k = i; k < j; k++)	//i~j 사이의 k값
 					DP[i][j] = MIN(DP[i][k] + DP[k+1][j] + sum(s, i, j), DP[i][j]);
-				}
 			}
 		}
 		return DP[0][a.length-1];
