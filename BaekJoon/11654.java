3c46384f41ee0c4b62fa1d3658cf3c8c02642419
akasai <ksj21c@naver.com>
2017-11-10 17:56:42 +0900

    Create 11654.java

---
+++ BaekJoon/11654.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+  public class Main{
+  public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+
+    int a = s.next().charAt(0);
+    System.out.println(String.format("%d", a));
+  }
+}
