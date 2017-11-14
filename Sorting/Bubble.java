75c0750fa196f8982687d2a0939e5574f26e84fd
akasai <ksj21c@naver.com>
2017-11-14 17:27:49 +0900

    Update Bubble.java

--- Sorting/Bubble.java
+++ Sorting/Bubble.java
@@ -3,18 +3,18 @@ import java.util.Random;
 public class Main{
   public static void main(String[] args){
     Random r = new Random();
-		int a[] = new int[10];
-		
-		for(int i = 0; i < 10; i++) a[i] = r.nextInt(100);
-		
-		bubble(a);
+    int a[] = new int[10];
+
+    for(int i = 0; i < 10; i++) a[i] = r.nextInt(100);
+
+    bubble(a);
   }
-  /*
-	 * Bubble Sorting
-	 * Best : O(n^2)
-	 * Avg  : O(n^2)
-	 * Worst: O(n^2)
-	 */
+/*
+ * Bubble Sorting
+ * Best : O(n^2)
+ * Avg  : O(n^2)
+ * Worst: O(n^2)
+ */
   private static void bubble(int[] a){
     int size = a.length;
     
